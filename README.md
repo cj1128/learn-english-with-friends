0eeb5c62f6a0bd47ba935e7812d3e4c4b72e1ce2
CJ Ting <fatelovely1128@gmail.com>
2019-05-16 19:33:30 +0800

    Update README

--- README.md
+++ README.md
@@ -1,33 +1,36 @@
 # Learn English with \`Friends\`
 
-排版好看的老友记剧本PDF文件，用于打印下来学习英语，晚上看剧，早上朗读，生活多美好。
+**排版好看并且内容准确**的老友记剧本 PDF 文件，用于打印下来学习英语。晚上看剧，早上读英语，幸福生活~
 
-最近开始重温老友记，感觉里面的对话很适合用来练习口语，很地道，早晨读一读更是再好不过。网上搜了一圈，找到的资料排版都很难看，So, Why not自己弄一个呢。
+最近开始重温老友记，感觉里面的对话很适合用来练习口语，很地道，早晨读一读更是再好不过。网上搜了一圈，找到的资料排版都很难看，So, Why not 自己弄一个呢。
 
-生成好的PDF文件和音频都在`dist`目录中，注意，因为[Prince]的缘故，PDF文件第一页有Prince的水印，虽然可以通过技术手段去除，但是我觉得保留下来也无伤大雅。
+生成好的 PDF 文件都在 `result` 目录中，注意，因为 [Prince] 的缘故，PDF 文件第一页有 Prince 的水印，虽然可以通过技术手段去除，但是我觉得保留下来更好。
 
-## 资源
+## 剧本参考
 
-- 剧本: https://fangj.github.io/friends/
-- 音频: http://x8.tingvoa.com//Sound/meiju/laoyouji/tingvoa.com_{target}.mp3
+- https://fangj.github.io/friends/
+- http://www.tvsubtitles.net/tvshow-65-1.html
 
-## PDF生成
+我在网上并没有找到完全正确的剧本，都有或多或少的瑕疵和错误。因此，最终我使用的剧本是在上面资源的基础上，通过反复听录音一遍遍校对得来的。
 
-调用`generate.py`，输入为文件或目录，结果写入到`dist`目录中，内部使用[Prince]完成PDF渲染。
+一个人的精力实在有限，**欢迎大家 Contribute!**
+
+## 音频
+
+推荐网易云音乐 [老友记全 10 季](https://music.163.com/#/playlist?id=102769145&userid=127057191)。
+
+
+## PDF 生成
+
+剧本文件名规范：`SxxExx.txt`，例如 `S01E01.txt`。
+
+调用 `generate.py`，输入为文件或目录，结果写入到 `result` 目录中。
 
 ```bash
 # 输入为某个具体文件
 $ ./generate.py src/S01/S01E01.txt
 # 输入也可以为一个目录，递归处理目录中的每一个文件
 $ ./generate.py src/S01
-```
-
-## 音频下载
-
-以第一季第一集为例。
-
-```bash
-$ wget http://x8.tingvoa.com//Sound/meiju/laoyouji/tingvoa.com_101.mp3 -O dist/S01E01.mp3
 ```
 
 [Prince]: https://www.princexml.com/
--- generate.py
+++ generate.py
@@ -2,7 +2,8 @@
 import re
 import sys
 from string import Template
-from os.path import isdir, join, basename, splitext
+from os.path import isdir, join, basename, splitext, abspath, dirname
+from pathlib import Path
 import glob
 import tempfile
 from subprocess import run
@@ -32,25 +33,34 @@ def format(line):
   return f"<p>{line}</p>"
 
 def process(path):
-  print(f"Process: {path}")
-  with open(path) as f:
-    lines = [line.strip() for line in f.read().split("\n") if line.strip() != ""]
-    content = "\n".join([format(l) for l in lines])
-    with tempfile.NamedTemporaryFile(mode="w+t", encoding="utf8") as tmp:
-      tmp.write(template.substitute(content=content))
-      tmp.flush()
-      name = splitext(basename(path))[0]
-      run(f"prince {tmp.name} -s pdf.css -o dist/{name}.pdf", shell=True, check=True)
-  print(f"Generate dist/{name}.pdf successfully.")
+  print(f"Process: { path }")
+
+  basename = path.name[:-4]
+  target_path = Path("result") / basename[:3] / f"{ basename }.pdf"
+
+  if target_path.exists():
+    print(f"{ target_path } Found")
+    return
+
+  lines = [line.strip() for line in path.read_text().split("\n") if line.strip() != ""]
+  content = "\n".join([format(l) for l in lines])
+
+  with tempfile.NamedTemporaryFile(mode="w+t", encoding="utf8") as tmp:
+    tmp.write(template.substitute(content=content))
+    tmp.flush()
+    target_path.parent.mkdir(parents=True, exist_ok=True)
+    run(f"prince { tmp.name } -s pdf.css -o { target_path }", shell=True, check=True)
+
+  print(f"Generate { target_path } successfully.")
 
 if len(sys.argv) < 2:
   print("usage: ./generate.py [file_or_dir]")
   exit(0)
 
-target = sys.argv[1]
+p = Path(sys.argv[1])
 
-if isdir(target):
-  for path in glob.glob(join(target, "**", "*.txt")):
-    process(path)
+if p.is_dir():
+  for file in p.rglob("*.txt"):
+    process(file)
 else:
-  process(target)
+  process(p)
